8227395ee2d1f1f4de53f5b90f92f8404d2de422
falafel-v-postel <zztopgrodno@gmail.com>
2018-03-01 15:44:51 +0300

    implementation solveEquation function

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ module.exports = function solveEquation(equation) {
   let b = parseInt(num[3]+num[4]);
   let a = parseInt(num[0]);
   let c = parseInt(num[7]+num[8]);
-  res[0] = Math.round((-1*b + Math.sqrt(Math.pow(b,2) - (4*a*c)))/(2*a));
-  res[1] = Math.round((-1*b - Math.sqrt(Math.pow(b,2) - (4*a*c)))/(2*a));
+  let diz = eval(Math.pow(b,2)-4*a*c)
+  res[0] = Math.round((-1*b + Math.sqrt(diz))/(2*a));
+  res[1] = Math.round((-1*b - Math.sqrt(diz))/(2*a));
   return res.sort((a,b)=>a-b);
 
 }
